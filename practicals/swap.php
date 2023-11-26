47672341bb1dd035a7fb46bdeb1cf8730f146dcd
Namrata Diware <namrata.diware@gmail.com>
2023-11-26 15:56:19 +0530

    Add swap code

--- practicals/swap.php
+++ practicals/swap.php
@@ -1,25 +1,22 @@
 <?php
 
-// Function to swap two numbers
-function swapNumbers(&$num1, &$num2) {
-    $temp = $num1;
-    $num1 = $num2;
-    $num2 = $temp;
+function swapNumbers($a, $b) {
+    // Print original numbers
+    echo "Original numbers: a = $a, b = $b\n";
+
+    // Swap the numbers
+    $temp = $a;
+    $a = $b;
+    $b = $temp;
+
+    // Print swapped numbers
+    echo "Swapped numbers: a = $a, b = $b\n";
 }
 
 // Example usage
 $number1 = 5;
 $number2 = 10;
 
-echo "Before swapping: \n";
-echo "Number 1: $number1 \n";
-echo "Number 2: $number2 \n";
-
-// Call the swap function
 swapNumbers($number1, $number2);
-
-echo "After swapping: \n";
-echo "Number 1: $number1 \n";
-echo "Number 2: $number2 \n";
 
 ?>
