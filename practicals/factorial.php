a55535314df7afb884c742ac8360a0ae3cf563ea
Namrata Diware <namrata.diware@gmail.com>
2023-11-26 16:19:13 +0530

    Add factorial code

--- practicals/factorial.php
+++ practicals/factorial.php
@@ -1,18 +1,27 @@
 <?php
 
-// Function to calculate factorial
-function factorial($number) {
+function calculateFactorial($number) {
+    // Base case: factorial of 0 is 1
     if ($number == 0 || $number == 1) {
         return 1;
     } else {
-        return $number * factorial($number - 1);
+        // Recursive case: factorial(n) = n * factorial(n-1)
+        return $number * calculateFactorial($number - 1);
     }
 }
 
-// Example usage
-$number = 5;
-$result = factorial($number);
+function printFactorial($number) {
+    // Check if the input is a non-negative integer
+    if (!is_numeric($number) || $number < 0 || floor($number) != $number) {
+        echo "Invalid input. Please enter a non-negative integer.";
+    } else {
+        // Calculate and print the factorial
+        $factorial = calculateFactorial($number);
+        echo "Factorial of $number is: $factorial";
+    }
+}
 
-echo "The factorial of $number is: $result";
+// Example: Calculate and print factorial of 5
+printFactorial(5);
 
 ?>
