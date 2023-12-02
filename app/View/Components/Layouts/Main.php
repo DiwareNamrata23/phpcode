c447e0271fc07cb07d4d61a7ea9240f7a13de75b
Namrata Diware <namrata.diware@gmail.com>
2023-12-02 11:13:06 +0530

    Customizes title of songs page

--- app/View/Components/Layouts/Main.php
+++ app/View/Components/Layouts/Main.php
@@ -16,8 +16,8 @@ class Main extends Component
      * Create a new component instance.
      */
     public function __construct(string $pageName = '') {
-        $this->pageName = $pageName;
-      }
+      $this->pageName = $pageName;
+    }
       
     /**
      * Get the view / contents that represent the component.
